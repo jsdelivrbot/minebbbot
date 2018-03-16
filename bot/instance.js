4d8b7af07accb576e9f86f955bef23ae297e55dd
meness <aesshoferi@gmail.com>
2018-03-16 13:50:32 +0330

    third

--- bot/instance.js
+++ bot/instance.js
@@ -1,6 +1,5 @@
-var TelegramBot = require('node-telegram-bot-api'),
-  bot = new TelegramBot(process.env.BOT_TOKEN, {
+const TelegramBot = require('node-telegram-bot-api');
+
+module.exports.bot = new TelegramBot(process.env.BOT_TOKEN, {
     polling: true
-  });
-  
-return bot;
+});
